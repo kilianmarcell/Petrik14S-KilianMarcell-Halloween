6c0d2d9650ca35eee1d98557dbdb6f5589ed3f09
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-05 14:36:38 +0100

    $resztvevok fixálva

--- src/Models/Esemeny.php
+++ src/Models/Esemeny.php
@@ -7,7 +7,7 @@ class Esemeny {
     private $resztvevok;
     private $ido;
 
-    public function __construct(string $nev, Lako $szerzo, $resztvevok = array(), DateTime $ido) {
+    public function __construct(string $nev, Lako $szerzo, array $resztvevok, DateTime $ido) {
         $this -> nev = $nev;
         $this -> szerzo = $szerzo;
         $this -> resztvevok = $resztvevok;
